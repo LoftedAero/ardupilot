c1a0b434469106028b247860d82a2a30a8659f79
LoftedAero <info@loftedaero.com>
2025-05-25 22:15:44 -0700

    Add F-35B frame class and motor mixer

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -376,6 +376,11 @@ void Copter::allocate_motors(void)
             motors_var_info = AP_MotorsTri::var_info;
             AP_Param::set_frame_type_flags(AP_PARAM_FRAME_TRICOPTER);
             break;
+        case AP_Motors::MOTOR_FRAME_F35B:
+            motors = new AP_MotorsF35B(copter.scheduler.get_loop_rate_hz());
+            motors_var_info = AP_MotorsF35B::var_info;
+            AP_Param::set_frame_type_flags(AP_PARAM_FRAME_TRICOPTER);
+            break;
         case AP_Motors::MOTOR_FRAME_SINGLE:
             motors = NEW_NOTHROW AP_MotorsSingle(copter.scheduler.get_loop_rate_hz());
             motors_var_info = AP_MotorsSingle::var_info;
@@ -494,6 +499,9 @@ void Copter::allocate_motors(void)
         attitude_control->get_rate_yaw_pid().kI().set_default(0.015);
         break;
     case AP_Motors::MOTOR_FRAME_TRI:
+        attitude_control->get_rate_yaw_pid().filt_D_hz().set_default(100);
+        break;
+    case AP_Motors::MOTOR_FRAME_F35B:
         attitude_control->get_rate_yaw_pid().filt_D_hz().set_default(100);
         break;
     default:
